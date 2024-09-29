32893500a355e73da32352b81a8ea5152c64d985
tanmaysalunke <tanmay442000@gmail.com>
2024-09-28 21:55:09 -0700

    check for session and user

--- Frontend/my-next-app/src/pages/dashboard.tsx
+++ Frontend/my-next-app/src/pages/dashboard.tsx
@@ -1,45 +1,45 @@
-import Link from 'next/link';
-import { signIn, signOut, useSession } from 'next-auth/react';
-import { useRouter } from 'next/router';
-import styles from '../styles/Home.module.css';
-import React, { useEffect } from 'react';
-
-
-
+import Link from "next/link";
+import { signIn, signOut, useSession } from "next-auth/react";
+import { useRouter } from "next/router";
+import styles from "../styles/Home.module.css";
+import React, { useEffect } from "react";
 
 export default function Dashboard() {
-    const { data: session,status } = useSession();
-    const router = useRouter();
-    console.log("Session Data:", session);
+  const { data: session, status } = useSession();
+  const router = useRouter();
+  console.log("Session Data:", session);
 
-    useEffect(() => {
-        // Check if the session is loaded and there is no user session
-        if (status === "unauthenticated") {
-            router.push('/api/auth/signin'); // Redirects to the sign-in page
-        }
-    }, [status, router]);
+  useEffect(() => {
+    // Check if the session is loaded and there is no user session
+    if (status === "unauthenticated") {
+      router.push("/api/auth/signin"); // Redirects to the sign-in page
+    }
+  }, [status, router]);
 
-    return (
-        <div className={styles.container}>
-            <main className={styles.main}>
-                <h1>Welcome to Dashboard</h1>
-                {session ? (
-                    <div>
-                        <p>Welcome, {session.user.name}! You are logged in.</p>
-                        <button onClick={() => signOut()} className={styles.formButton}>
-                            Log Out
-                        </button>
-                        <button onClick={() => signIn('google')} className={styles.formButton}>
-                            Sign in to AWS SSO
-                        </button>
-                        {/* Include other user-specific or SSO actions here */}
-                    </div>
-                ) : (
-                    <div>
-                            <p>Redirecting to login...</p>
-                    </div>
-                )}
-            </main>
-        </div>
-    );
+  return (
+    <div className={styles.container}>
+      <main className={styles.main}>
+        <h1>Welcome to Dashboard</h1>
+        {session ? (
+          <div>
+            <p>Welcome, {session?.user?.name}! You are logged in.</p>
+            <button onClick={() => signOut()} className={styles.formButton}>
+              Log Out
+            </button>
+            <button
+              onClick={() => signIn("google")}
+              className={styles.formButton}
+            >
+              Click Here to set up AWS Monitoring
+            </button>
+            {/* Include other user-specific or SSO actions here */}
+          </div>
+        ) : (
+          <div>
+            <p>Redirecting to login...</p>
+          </div>
+        )}
+      </main>
+    </div>
+  );
 }
